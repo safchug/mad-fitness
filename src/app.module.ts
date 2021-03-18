502d2709274fbc2b5afe7fa96d2730b74fe7b59c
TARAS <taraskomaryshyn@gmail.com>
2021-03-18 15:52:35 +0200

    add changes in modul files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
 import { RefreshTokensModule } from './refresh-tokens/refresh-tokens.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RolesModule, RefreshTokensModule],
+  imports: [RolesModule, RefreshTokensModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/refresh-tokens/refresh-tokens.module.ts
+++ src/refresh-tokens/refresh-tokens.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RefreshTokensController } from './refresh-tokens.controller';
 import { RefreshTokensService } from './refresh-tokens.service';
+import { RefreshTokensEntity } from './entity/refresh-tokens.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RefreshTokensEntity])],
   controllers: [RefreshTokensController],
-  providers: [RefreshTokensService]
+  providers: [RefreshTokensService],
 })
 export class RefreshTokensModule {}
