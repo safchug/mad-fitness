a842f34ac9b2fab7798761d2b9e888c32be298d9
Vasyl Savchuk <safchug@gmail.com>
2021-03-18 18:37:20 +0200

    fixed a little bit

--- src/shedule/entity/shedule.entity.ts
+++ src/shedule/entity/shedule.entity.ts
@@ -1,15 +1,17 @@
-import { Column, CreateDateColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import User from '';
-import ClassEntity from "../../classes/enteties/class.entity";
+import ClassEntity from "../../classes/entity/class.entity";
 
 class SheduleEntity {
   @PrimaryGeneratedColumn({name: 'id'})
   id: number;
 
   @OneToOne(() => User, user => user.id)
+  @JoinColumn()
   trener: User;
 
   @OneToOne(() => ClassEntity, classEntity => classEntity.id)
+  @JoinColumn()
   class: ClassEntity;
 
   @Column({name: 'description'})
@@ -21,7 +23,7 @@ class SheduleEntity {
   @CreateDateColumn({name: "start_date", type: "timestamp"})
   startDate: Date;
 
-  @CreateDateColumn({name: "end_date", type: "timestamp"})
+  @Column({name: "end_date", type: "timestamp"})
   endDate: Date;
 
   @CreateDateColumn({name: "created_at", type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)"})
