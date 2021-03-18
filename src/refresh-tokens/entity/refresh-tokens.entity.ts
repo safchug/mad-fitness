2692c3140a809c85bb828044d8b67fda6efc68ea
TARAS <taraskomaryshyn@gmail.com>
2021-03-18 15:57:30 +0200

    add changes in entity files

--- src/refresh-tokens/entity/refresh-tokens.entity.ts
+++ src/refresh-tokens/entity/refresh-tokens.entity.ts
@@ -22,9 +22,9 @@ export class RefreshTokensEntity {
   @Column({ type: 'varchar', length: 255, nullable: false })
   token: string;
 
-  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at', default: () => "CURRENT_TIMESTAMP(6)"})
   createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at', default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
   updatedAt: Date;
 }
--- src/roles/entity/roles.entity.ts
+++ src/roles/entity/roles.entity.ts
@@ -14,9 +14,9 @@ export class RolesEntity {
   @Column({ type: 'varchar', length: 50, nullable: false, unique: true })
   role: string;
 
-  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at', default: () => "CURRENT_TIMESTAMP(6)"})
   createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at', default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
   updatedAt: Date;
 }
