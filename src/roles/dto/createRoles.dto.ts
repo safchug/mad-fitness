f9ba4d1caa119dd944b9c1de3ae55f78bcf42cb6
TARAS <taraskomaryshyn@gmail.com>
2021-04-09 17:33:00 +0300

    just commit

--- src/roles/dto/createRoles.dto.ts
+++ src/roles/dto/createRoles.dto.ts
@@ -1,8 +1,13 @@
 import { IsString, IsNotEmpty, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateRolesDto {
   @IsNotEmpty()
   @IsString()
   @Length(1, 50)
+  @ApiProperty({
+    example: 'new_role',
+    type: String,
+  })
   role: string;
 }
